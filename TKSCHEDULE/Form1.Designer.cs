fa010264ecfcd779a335bff1fc5fd28b20cd5c2f
at3158 <tk290@tkfood.com.tw>
2016-09-07 16:47:47 +0800

    INSERT INTO [HRMDB].[dbo].[AttendanceRollcall]

--- TKSCHEDULE/Form1.Designer.cs
+++ TKSCHEDULE/Form1.Designer.cs
@@ -28,20 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(784, 561);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(776, 535);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "HR";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(776, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGreen;
+            this.tableLayoutPanel1.ColumnCount = 10;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 4, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 15;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(770, 529);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label1, 5);
+            this.label1.Location = new System.Drawing.Point(3, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TIME";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 3);
+            this.label2.Location = new System.Drawing.Point(83, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "是否啟動自動打卡(每日8點執行)";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(3, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "啟動";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GO";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TKSCHEDULE/Form1.cs
+++ TKSCHEDULE/Form1.cs
@@ -1,20 +1,129 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Media;
 
 namespace TKSCHEDULE
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlConnection sqlConn2 = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        int result;
+        string HRAUTO = "N";
+
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        #region FUNCTION
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+            HRAUTORUN();
+
+        }
+        public void SETbutton1()
+        {
+            if(button1.Text.Equals("啟動"))
+            {
+                HRAUTO = "Y";
+                button1.Text = "停止";
+                button1.ForeColor = Color.Red;
+            }
+            else if(button1.Text.Equals("停止"))
+            {
+                HRAUTO = "N";
+                button1.Text = "啟動";
+                button1.ForeColor = Color.Blue;
+            }
+        }
+        public void HRAUTORUN()
+        {
+            string hh = "16";
+            string mm = "45";
+            if (HRAUTO.Equals("Y")&& DateTime.Now.Hour.ToString().Equals(hh)&&DateTime.Now.Minute.ToString().Equals(mm))
+            {
+                label3.Text = "YYYYYYY";
+                ADDHRCARD();
+            }
+            else
+            {
+                label3.Text = "GO";
+            }     
+            
+        }
+        public void ADDHRCARD()
+        {
+            DateTime workdt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day+1, 8, 30, 0);
+            DateTime workdt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 17, 30, 0);
+            DateTime carddt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 8, 20, 0);            DateTime carddt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 18, 30, 0);
+            DateTime operdat = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 09, 10, 0);
+
+            sbSql.Clear();
+            sbSql.Append(" INSERT INTO [HRMDB].[dbo].[AttendanceRollcall] ([AttendanceRollcallId],[EmployeeId],[Date],[BeginTime],[EndTime],[AttendanceRankId],[AttendanceTypeId],[Hours],[QuartersHours],[QuartersHoursUnit],[IsConfirm],[OperationDate],[UserId],[Recover],[Remark],[CreateDate],[LastModifiedDate],[CreateBy],[LastModifiedBy],[CorporationId],[Flag],[AssignReason],[OwnerId],[VirObjectId],[ActualBeginTime],[ActualEndTime],[Count],[DailyCards],[EmpRankCards],[CollectBegin],[CollectEnd],[IsAbnormal]) ");
+            sbSql.AppendFormat(" SELECT  '{0}' AS [AttendanceRollcallId]", Guid.NewGuid());
+            sbSql.Append(" , [Employee].EmployeeId AS [EmployeeId]");
+            sbSql.AppendFormat(" ,'{0}' AS [Date]", workdt1.ToString("yyyy-MM-dd 00:00:00"));
+            sbSql.AppendFormat(" ,'{0}' AS [BeginTime]", workdt1.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.AppendFormat(" ,'{0}' AS [EndTime]", workdt2.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceRankId]");
+            sbSql.Append(" ,'R13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceTypeId]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [Hours]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [QuartersHours]");
+            sbSql.Append(" ,'AttendanceUnit_003' AS [QuartersHoursUnit]");
+            sbSql.Append(" ,'0' AS [IsConfirm]");
+            sbSql.AppendFormat(" ,'{0}' AS [OperationDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [UserId]");
+            sbSql.Append(" ,'1' AS [Recover]");
+            sbSql.Append(" ,NULL AS [Remark]");
+            sbSql.AppendFormat(" ,'{0}' AS [CreateDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss")); ;
+            sbSql.AppendFormat(" ,'{0}' AS [LastModifiedDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [CreateBy]");
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [LastModifiedBy]");
+            sbSql.Append(" ,NULL AS [CorporationId]");
+            sbSql.Append(" ,'1' AS [Flag]");
+            sbSql.Append(" ,NULL AS [AssignReason]");
+            sbSql.Append(" ,'c9aec8c3-8889-4a8b-b718-9ce89aa84b22' AS [OwnerId]");
+            sbSql.Append(" ,NULL AS [VirObjectId]");
+            sbSql.Append(" ,NULL AS [ActualBeginTime]");
+            sbSql.Append(" ,NULL AS [ActualEndTime]");
+            sbSql.Append(" ,'1' AS [Count]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END  AS [DailyCards]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END AS [EmpRankCards]");
+            sbSql.AppendFormat(" ,'{0}' AS [CollectBegin]",carddt1.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.AppendFormat(" ,'{0}' AS [CollectEnd]", carddt2.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'0' AS [IsAbnormal]");
+            sbSql.Append(" FROM [HRMDB].[dbo].[Employee]");
+            sbSql.Append(" WHERE  [Employee].EmployeeId='C9AEC8C3-8889-4A8B-B718-9CE89AA84B22'");
+            sbSql.Append(" ");
+        }
+        #endregion
+
+        #region BUTTON
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SETbutton1();
         }
     }
 }
