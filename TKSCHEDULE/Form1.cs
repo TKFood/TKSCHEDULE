968a0b8e2aad4ff92a94251260d5407e0e094ad6
at3158 <tk290@tkfood.com.tw>
2016-09-08 08:43:58 +0800

    modifer date

--- TKSCHEDULE/Form1.cs
+++ TKSCHEDULE/Form1.cs
@@ -57,8 +57,8 @@ namespace TKSCHEDULE
         }
         public void HRAUTORUN()
         {
-            string hh = "17";
-            string mm = "36";
+            string hh = "8";
+            string mm = "41";
             if (HRAUTO.Equals("Y")&& DateTime.Now.Hour.ToString().Equals(hh)&&DateTime.Now.Minute.ToString().Equals(mm))
             {
                 label3.Text = DateTime.Now.ToString("yyyymmdd") + " DONE";
@@ -69,51 +69,52 @@ namespace TKSCHEDULE
         }
         public void ADDHRCARD()
         {
-            DateTime workdt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day+1, 8, 30, 0);
-            DateTime workdt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 17, 30, 0);
-            DateTime carddt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 8, 20, 0);            DateTime carddt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 18, 30, 0);
-            DateTime operdat = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 09, 10, 0);
-            DateTime operdat2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 09, 10, 0);
+            DateTime workdt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 30, 0);
+            DateTime workdt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day , 17, 30, 0);
+            DateTime carddt1 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day , 8, 20, 0);
+            DateTime carddt2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day , 18, 30, 0);
+            DateTime operdat = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day , 09, 10, 0);
+            DateTime operdat2 = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day , 09, 10, 0);
 
             sbSql.Clear();
-            //sbSql.Append(" INSERT INTO [HRMDB].[dbo].[AttendanceRollcall] ([AttendanceRollcallId],[EmployeeId],[Date],[BeginTime],[EndTime],[AttendanceRankId],[AttendanceTypeId],[Hours],[QuartersHours],[QuartersHoursUnit],[IsConfirm],[OperationDate],[UserId],[Recover],[Remark],[CreateDate],[LastModifiedDate],[CreateBy],[LastModifiedBy],[CorporationId],[Flag],[AssignReason],[OwnerId],[VirObjectId],[ActualBeginTime],[ActualEndTime],[Count],[DailyCards],[EmpRankCards],[CollectBegin],[CollectEnd],[IsAbnormal]) ");
-            //sbSql.AppendFormat(" SELECT  '{0}' AS [AttendanceRollcallId]", Guid.NewGuid());
-            //sbSql.Append(" , [Employee].EmployeeId AS [EmployeeId]");
-            //sbSql.AppendFormat(" ,'{0}' AS [Date]", workdt1.ToString("yyyy-MM-dd 00:00:00"));
-            //sbSql.AppendFormat(" ,'{0}' AS [BeginTime]", workdt1.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.AppendFormat(" ,'{0}' AS [EndTime]", workdt2.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.Append(" ,'13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceRankId]");
-            //sbSql.Append(" ,'R13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceTypeId]");
-            //sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [Hours]");
-            //sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [QuartersHours]");
-            //sbSql.Append(" ,'AttendanceUnit_003' AS [QuartersHoursUnit]");
-            //sbSql.Append(" ,'0' AS [IsConfirm]");
-            //sbSql.AppendFormat(" ,'{0}' AS [OperationDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [UserId]");
-            //sbSql.Append(" ,'1' AS [Recover]");
-            //sbSql.Append(" ,NULL AS [Remark]");
-            //sbSql.AppendFormat(" ,'{0}' AS [CreateDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss")); ;
-            //sbSql.AppendFormat(" ,'{0}' AS [LastModifiedDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [CreateBy]");
-            //sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [LastModifiedBy]");
-            //sbSql.Append(" ,NULL AS [CorporationId]");
-            //sbSql.Append(" ,'1' AS [Flag]");
-            //sbSql.Append(" ,NULL AS [AssignReason]");
-            //sbSql.Append(" ,'c9aec8c3-8889-4a8b-b718-9ce89aa84b22' AS [OwnerId]");
-            //sbSql.Append(" ,NULL AS [VirObjectId]");
-            //sbSql.Append(" ,NULL AS [ActualBeginTime]");
-            //sbSql.Append(" ,NULL AS [ActualEndTime]");
-            //sbSql.Append(" ,'1' AS [Count]");
-            //sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END  AS [DailyCards]");
-            //sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END AS [EmpRankCards]");
-            //sbSql.AppendFormat(" ,'{0}' AS [CollectBegin]",carddt1.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.AppendFormat(" ,'{0}' AS [CollectEnd]", carddt2.ToString("yyyy-MM-dd HH:mm:ss"));
-            //sbSql.Append(" ,'0' AS [IsAbnormal]");
-            //sbSql.Append(" FROM [HRMDB].[dbo].[Employee]");
-            //sbSql.Append(" WHERE  [Employee].EmployeeId='C9AEC8C3-8889-4A8B-B718-9CE89AA84B22'");
-            //sbSql.Append(" ");
+            sbSql.Append(" INSERT INTO [HRMDB].[dbo].[AttendanceRollcall] ([AttendanceRollcallId],[EmployeeId],[Date],[BeginTime],[EndTime],[AttendanceRankId],[AttendanceTypeId],[Hours],[QuartersHours],[QuartersHoursUnit],[IsConfirm],[OperationDate],[UserId],[Recover],[Remark],[CreateDate],[LastModifiedDate],[CreateBy],[LastModifiedBy],[CorporationId],[Flag],[AssignReason],[OwnerId],[VirObjectId],[ActualBeginTime],[ActualEndTime],[Count],[DailyCards],[EmpRankCards],[CollectBegin],[CollectEnd],[IsAbnormal]) ");
+            sbSql.AppendFormat(" SELECT  '{0}' AS [AttendanceRollcallId]", Guid.NewGuid());
+            sbSql.Append(" , [Employee].EmployeeId AS [EmployeeId]");
+            sbSql.AppendFormat(" ,'{0}' AS [Date]", workdt1.ToString("yyyy-MM-dd 00:00:00"));
+            sbSql.AppendFormat(" ,'{0}' AS [BeginTime]", workdt1.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.AppendFormat(" ,'{0}' AS [EndTime]", workdt2.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceRankId]");
+            sbSql.Append(" ,'R13716c6bf390da08b46d1b85e1c1d24d986c9' AS[AttendanceTypeId]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [Hours]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN 0  WHEN '7' THEN 0 ELSE 480 END  AS [QuartersHours]");
+            sbSql.Append(" ,'AttendanceUnit_003' AS [QuartersHoursUnit]");
+            sbSql.Append(" ,'0' AS [IsConfirm]");
+            sbSql.AppendFormat(" ,'{0}' AS [OperationDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [UserId]");
+            sbSql.Append(" ,'1' AS [Recover]");
+            sbSql.Append(" ,NULL AS [Remark]");
+            sbSql.AppendFormat(" ,'{0}' AS [CreateDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss")); ;
+            sbSql.AppendFormat(" ,'{0}' AS [LastModifiedDate]", operdat.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [CreateBy]");
+            sbSql.Append(" ,'FC0D07EA-E0FD-4BCF-B127-22F83D63D834' AS [LastModifiedBy]");
+            sbSql.Append(" ,NULL AS [CorporationId]");
+            sbSql.Append(" ,'1' AS [Flag]");
+            sbSql.Append(" ,NULL AS [AssignReason]");
+            sbSql.Append(" ,'c9aec8c3-8889-4a8b-b718-9ce89aa84b22' AS [OwnerId]");
+            sbSql.Append(" ,NULL AS [VirObjectId]");
+            sbSql.Append(" ,NULL AS [ActualBeginTime]");
+            sbSql.Append(" ,NULL AS [ActualEndTime]");
+            sbSql.Append(" ,'1' AS [Count]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END  AS [DailyCards]");
+            sbSql.Append(" ,CASE datepart(weekday, getdate()) WHEN '1' THEN NULL  WHEN '7' THEN NULL ELSE ' 08:20| 18:30' END AS [EmpRankCards]");
+            sbSql.AppendFormat(" ,'{0}' AS [CollectBegin]", carddt1.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.AppendFormat(" ,'{0}' AS [CollectEnd]", carddt2.ToString("yyyy-MM-dd HH:mm:ss"));
+            sbSql.Append(" ,'0' AS [IsAbnormal]");
+            sbSql.Append(" FROM [HRMDB].[dbo].[Employee]");
+            sbSql.Append(" WHERE  [Employee].EmployeeId='C9AEC8C3-8889-4A8B-B718-9CE89AA84B22'");
+            sbSql.Append(" ");
 
-            if(DateTime.Now.DayOfWeek.ToString("d").Equals("1")|| DateTime.Now.DayOfWeek.ToString("d").Equals("1")|| DateTime.Now.DayOfWeek.ToString("d").Equals("2")|| DateTime.Now.DayOfWeek.ToString("d").Equals("3")|| DateTime.Now.DayOfWeek.ToString("d").Equals("4")|| DateTime.Now.DayOfWeek.ToString("d").Equals("5"))
+            if (DateTime.Now.DayOfWeek.ToString("d").Equals("1")|| DateTime.Now.DayOfWeek.ToString("d").Equals("1")|| DateTime.Now.DayOfWeek.ToString("d").Equals("2")|| DateTime.Now.DayOfWeek.ToString("d").Equals("3")|| DateTime.Now.DayOfWeek.ToString("d").Equals("4")|| DateTime.Now.DayOfWeek.ToString("d").Equals("5"))
             {
                 sbSql.Append(" INSERT INTO [HRMDB].[dbo].[AttendanceCollect] ([AttendanceCollectId],[MachineId],[MachineCode],[CardId],[CardCode],[EmployeeName],[EmployeeCode],[EmployeeId],[DepartmentName],[DepartmentId],[CostCenterId],[CostCenterCode],[Date],[Time],[IsManual],[Source],[IsUnusual],[UnusualTypeId],[Remark],[CreateDate],[LastModifiedDate],[CreateBy],[LastModifiedBy],[CorporationId],[Flag],[RepairId],[AttendanceTypeId],[OldLogIds],[AttendanceCollectLogId],[AssignReason],[OwnerId],[IsEss],[IsEF],[EssNo],[EssType],[ClassCode],[SubmitOperationDate],[SubmitUserId],[ConfirmOperationDate],[ConfirmUserId],[ApproveResultId],[FoundOperationDate],[FoundUserId],[ApproveDate],[ApproveEmployeeId],[ApproveEmployeeName],[ApproveRemark],[ApproveOperationDate],[ApproveUserId],[RepealOperationDate],[RepealUserId],[StateId],[IsFromEss],[IsForAttendance] )");
                 sbSql.AppendFormat(" SELECT '{0}' AS [AttendanceCollectId]", Guid.NewGuid());
@@ -131,7 +132,7 @@ namespace TKSCHEDULE
                 sbSql.AppendFormat(" ,'{0}' AS [Date]", carddt1.ToString("yyyy-MM-dd HH:mm:ss"));
                 sbSql.AppendFormat(" ,'{0}' AS [Time]", carddt1.ToString("HH:mm"));
                 sbSql.Append(" ,'0' AS [IsManual]");
-                sbSql.AppendFormat(" ,'{0} '+[CardNo]+' 03' AS [Source]", carddt1.ToString("yyyy/MM/dd"));
+                sbSql.AppendFormat(" ,'{0} '+[CardNo]+' 03' AS [Source]", carddt1.ToString("yyyy/MM/dd HH:mm"));
                 sbSql.Append(" ,'0' AS [IsUnusual]");
                 sbSql.Append(" ,NULL AS [UnusualTypeId]");
                 sbSql.Append(" ,NULL AS [Remark]");
@@ -191,7 +192,7 @@ namespace TKSCHEDULE
                 sbSql.AppendFormat(" ,'{0}' AS [Date]", carddt2.ToString("yyyy-MM-dd HH:mm:ss"));
                 sbSql.AppendFormat(" ,'{0}' AS [Time]", carddt2.ToString("HH:mm"));
                 sbSql.Append(" ,'0' AS [IsManual]");
-                sbSql.AppendFormat(" ,'{0} '+[CardNo]+' 03' AS [Source]", carddt2.ToString("yyyy/MM/dd"));
+                sbSql.AppendFormat(" ,'{0} '+[CardNo]+' 03' AS [Source]", carddt2.ToString("yyyy/MM/dd  HH:mm"));
                 sbSql.Append(" ,'0' AS [IsUnusual]");
                 sbSql.Append(" ,NULL AS [UnusualTypeId]");
                 sbSql.Append(" ,NULL AS [Remark]");
