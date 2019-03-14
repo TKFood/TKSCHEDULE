4918412aeb6f804e49b929b696df5cdb13668d6d
at3158 <tk290@tkfood.com.tw>
2019-03-14 10:45:53 +0800

    chang

--- TKSCHEDULE/Form1.Designer.cs
+++ TKSCHEDULE/Form1.Designer.cs
@@ -141,7 +141,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(234, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "是否啟動自動打卡";
+            this.label2.Text = "6:10是否啟動自動打卡";
             // 
             // label3
             // 
